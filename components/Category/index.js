d0a2985df9de78434d575910598dac0ffae34be0
reubn <4957964+reubn@users.noreply.github.com>
2020-11-25 19:17:47 +0000

    🐛

--- components/Category/index.js
+++ components/Category/index.js
@@ -7,5 +7,5 @@ import categories from '../../content/categories'
 import {category as categoryStyles} from './styles'
 
 export default ({category}) => (
-  <ActiveLink href={`/categories/${category}`} passHref>{active => <a className={categoryStyles} style={{'--category-background': categories[category]}}>{category}</a>}</ActiveLink>
+  <ActiveLink key={category} href={`/categories/${category}`} passHref>{active => <a className={categoryStyles} style={{'--category-background': categories[category]}}>{category}</a>}</ActiveLink>
 )
