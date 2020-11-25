879afe6cac59f91d9a3296b48bedb1fd69907422
reubn <4957964+reubn@users.noreply.github.com>
2020-11-25 20:00:33 +0000

    🧹

--- src/syntaxHighlight/rehype.js
+++ src/syntaxHighlight/rehype.js
@@ -1,58 +1,40 @@
-'use strict';
+// https://github.com/mapbox/rehype-prism/blob/main/index.js
 
-const visit = require('unist-util-visit');
-const nodeToString = require('hast-util-to-string');
-const refractor = require('refractor');
+const visit = require('unist-util-visit')
+const nodeToString = require('hast-util-to-string')
+const refractor = require('refractor')
 
 require('./languages')(refractor)
 
-module.exports = (options) => {
-  options = options || {};
-
-  return (tree) => {
-    visit(tree, 'element', visitor);
-  };
-
-  function visitor(node, index, parent) {
-    if (!(node.tagName === 'inlineCode' || (node.tagName === 'code' && parent && parent.tagName === 'pre'))) {
-      return;
-    }
-
-    const lang = getLanguage(node);
-
-    if (lang === null) {
-      return;
-    }
-
-    let result;
-    try {
-      const properties = {
-        className: (parent.properties.className || []).concat(
-          'language-' + lang
-        ),
-        metastring: node.properties.metastring
-      }
-
-      if(parent.tagName === 'pre') parent.properties = properties
-      node.properties = properties
-
-      result = refractor.highlight(nodeToString(node), lang);
-    } catch (err) {
-      return console.error(err)
-    }
-
-    node.children = result;
-  }
-};
-
 function getLanguage(node) {
-  const className = node.properties.className || node.className || [];
+  const className = node.properties.className || node.className || []
 
   for (const classListItem of className) {
-    if (classListItem.slice(0, 9) === 'language-') {
-      return classListItem.slice(9).toLowerCase();
-    }
+    if(classListItem.slice(0, 9) === 'language-') return classListItem.slice(9).toLowerCase()
   }
 
-  return null;
+  return null
 }
+
+function visitor(node, index, parent) {
+  if (!(node.tagName === 'inlineCode' || (node.tagName === 'code' && parent && parent.tagName === 'pre'))) return
+
+  const lang = getLanguage(node)
+  if (lang === null) return
+
+  const properties = {
+    className: (parent.properties.className || []).concat('language-' + lang),
+    metastring: node.properties.metastring
+  }
+
+  if(parent.tagName === 'pre') parent.properties = properties
+  node.properties = properties
+
+  let result
+  try {result = refractor.highlight(nodeToString(node), lang)}
+  catch (err) {return console.error(err)}
+
+  node.children = result
+}
+
+module.exports = () => tree => visit(tree, 'element', visitor)
--- src/syntaxHighlight/remark.js
+++ src/syntaxHighlight/remark.js
@@ -1,30 +1,17 @@
 // https://github.com/stefanprobst/remark-inline-code-language/blob/master/index.js
 
-var visit = require('unist-util-visit');
+const visit = require('unist-util-visit')
 
-module.exports = attacher;
-attacher.withInlineCodeLanguage = withInlineCodeLanguage;
-
-const regex = /^\:\{(.+?)\}/
-
-function attacher(separator) {
-  function transformer(tree) {
-    visit(tree, 'inlineCode', function visitor(node) {
-      return withInlineCodeLanguage(node);
-    });
-  }
-
-  return transformer;
-}
+const regex = /^\:\{(.+?)\}\s?/ // `{:js} console.log('this is js')`
 
 function withInlineCodeLanguage(node) {
   const match = node.value.match(regex)
 
-  const language = match ? match[1] : undefined;
-  if (language) {
-    node.value = node.value.replace(regex, '');
-    node.className = [`language-${language.trim()}`]
+  const language = match ? match[1] : undefined
+  if(language) {
+    node.value = node.value.replace(regex, '')
+    node.className = [`language-${language.trim().toLowerCase()}`]
   }
-
-  return node;
 }
+
+module.exports = () => tree => visit(tree, 'inlineCode', node => withInlineCodeLanguage(node))
