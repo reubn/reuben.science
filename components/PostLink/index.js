63af0c668e1c86daef5615e279b5871ca61d15ed
reubn <4957964+reubn@users.noreply.github.com>
2020-11-25 21:03:14 +0000

    ✏️

--- components/PostLink/index.js
+++ components/PostLink/index.js
@@ -1,13 +1,13 @@
 import ActiveLink from '../ActiveLink'
 
-import {post as postStyle, title as titleStyle, description as descriptionStyle, emoji as emojiStyle} from './styles'
+import {post as postStyle, title as titleStyle, subtitle as subtitleStyle, emoji as emojiStyle} from './styles'
 
-export default ({slug, metadata: {title, description, emoji}}) => {
+export default ({slug, metadata: {title, subtitle, emoji}}) => {
   const post = active => (
     <a>
       <div className={postStyle}>
         <p className={titleStyle}><span className={emojiStyle}>{emoji}</span>{title}</p>
-        <p className={descriptionStyle}>{description || ''}</p>
+        <p className={subtitleStyle}>{subtitle || ''}</p>
       </div>
     </a>
   )
