39c1d5b5eedf43030faabae9bec278884ce8e1c9
reubn <4957964+reubn@users.noreply.github.com>
2020-11-26 16:27:24 +0000

    📁

--- content/categories.js
+++ content/categories.js
@@ -1,5 +1,6 @@
 export default {
   hardware: 'linear-gradient(135deg, #FF52B0 0%, #FC275F 100%)',
   medicine: 'linear-gradient(135deg, #0CE9E8 0%, #04A5FB 100%)',
-  dev: 'linear-gradient(135deg, #0CE9E8 0%, #04A5FB 100%)'
+  dev: 'linear-gradient(135deg, #0CE9E8 0%, #04A5FB 100%)',
+  cooking: 'linear-gradient(135deg, #FF52B0 0%, #FC275F 100%)'
 }
