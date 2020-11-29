205c837284624dc6281774703b84fb7de6a8753a
reubn <4957964+reubn@users.noreply.github.com>
2020-11-29 13:31:22 +0000

    🔥 Better Unsplash Quality

--- src/photos.js
+++ src/photos.js
@@ -5,5 +5,5 @@ const response = fetch(`https://api.unsplash.com/users/${process.env.UNSPLASH_US
 export default async () => {
 	const json = await response
 
-  return json.map(({id, urls: {small}}) => ({src: small, id}))
+  return json.map(({id, urls: {raw}}) => ({src: `${raw}?q=${process.env.UNSPLASH_QUALITY}&fm=jpg&w=${process.env.UNSPLASH_SIZE}&fit=max`, id}))
 }
