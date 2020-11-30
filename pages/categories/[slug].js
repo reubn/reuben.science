fdaab83eb15b1faed1b2f8ad7599dbefb0374bf9
reubn <4957964+reubn@users.noreply.github.com>
2020-11-30 12:35:23 +0000

    🧹

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -64,7 +64,7 @@ export const getStaticProps = async ctx => {
   }
 }
 
-export async function getStaticPaths() {
+export function getStaticPaths() {
 
   return {
     paths: Object.keys(categories).map(slug => ({params: {slug}})),
