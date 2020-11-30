00fcafeb325904f01486f0a67d349e311f0ec49f
reubn <4957964+reubn@users.noreply.github.com>
2020-11-30 12:12:43 +0000

    🔥 CategoryLink Inherit Class

--- components/CategoryLink/index.js
+++ components/CategoryLink/index.js
@@ -4,6 +4,6 @@ import categories from '../../content/categories'
 
 import {category as categoryStyles} from './styles'
 
-export default ({category}) => (
-  <ActiveLink key={category} href={`/categories/${category}`} passHref>{active => <a className={categoryStyles} style={{'--category-background': categories[category]}}>{category}</a>}</ActiveLink>
+export default ({category, className='', ...props}) => (
+  <ActiveLink key={category} href={`/categories/${category}`} passHref>{active => <a className={[className, categoryStyles].join(' ')} style={{'--category-background': categories[category]}}>{category}</a>}</ActiveLink>
 )
