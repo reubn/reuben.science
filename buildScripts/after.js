8de43fcc7dd939ee145569ce742fc9dc6b7d4984
reubn <4957964+reubn@users.noreply.github.com>
2020-12-02 15:45:24 +0000

    🧹

--- buildScripts/after.js
+++ buildScripts/after.js
@@ -1,10 +1,10 @@
 const env = require('@next/env')
 
+env.loadEnvConfig(process.cwd(), process.env.NODE_ENV === 'development')
+
 const extractPages = require('./extractPages')
 const generateSitemap = require('./generateSitemap')
 const generateRSS = require('./generateRSS')
-
-env.loadEnvConfig(process.cwd(), process.env.NODE_ENV === 'development')
 
 const pages = extractPages()
 generateSitemap(pages)
