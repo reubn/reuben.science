4d02fb5e1deb1362de3c42c54cc0fe7581afb957
Reuben <4957964+reubn@users.noreply.github.com>
2020-12-07 15:59:43 +0000

    🔥 Placeholder

--- components/Post/Image/index.js
+++ components/Post/Image/index.js
@@ -1,8 +1,14 @@
+import {useState} from 'react'
+
+import {loading} from './styles'
+
 import Lazy from '../../Lazy'
 
-const Image = ({src, ...props}) => {
+const Image = ({src, className, ...props}) => {
   const densities = Array.isArray(src) ? src : [src]
   const srcset = densities.map((s, i) => `${s} ${i + 1}x`).join(', ')
+
+  const [loaded, setLoaded] = useState(!process.browser)
 
   return (
     <Lazy>
@@ -10,11 +16,14 @@ const Image = ({src, ...props}) => {
         <img
           ref={_ref}
 
+          onLoad={() => setLoaded(true)}
+
           src={inView ? densities[0] : ''}
           srcSet={inView ? srcset : ''}
 
           width={src.meta.width || 1000}
           height={src.meta.height || 0}
+          className={loaded ? className : [loading, className].join(' ')}
           loading="lazy"
           {...props}
         />
--- components/Post/Video/index.js
+++ components/Post/Video/index.js
@@ -1,6 +1,10 @@
+import {useState} from 'react'
 import Lazy from '../../Lazy'
 
-const Video = ({srcSet, ...props}) => {
+import {loading} from './styles'
+
+const Video = ({srcSet, className, ...props}) => {
+  const [loaded, setLoaded] = useState(!process.browser)
 
   return (
     <Lazy>
@@ -8,8 +12,13 @@ const Video = ({srcSet, ...props}) => {
         <video
           ref={_ref}
           key={inView ? 1 : 0}
+
+          onLoadedData={() => setLoaded(true)}
+
           width={srcSet.meta.width || 1000}
           height={srcSet.meta.height || 0}
+
+          className={loaded ? className : [loading, className].join(' ')}
 
           {...props}>
           {srcSet.map(({src, ...srcProps}) => <source src={inView ? src : null} {...srcProps} />)}
