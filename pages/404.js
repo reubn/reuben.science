ad593f74024db6c655d2621ff0677d101ea8ca6a
Reuben <4957964+reubn@users.noreply.github.com>
2021-01-03 12:32:07 +0000

    🔥404 Update

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,15 @@
 import PostList from '@/components/PostList'
+import PhotoList from '@/components/PhotoList'
 
 import {main, heading, posts as postsStyle, postLink} from './styles'
 import {errorCode, errorMessage} from './404.module.css'
 
 import sortPosts from '@/src/sortPosts'
+import photos from '@/src/photos'
 
 import * as postsFns from './posts/[slug].js'
 
-export default function Home({posts}) {
+export default function Home({posts, photos, photoSize}) {
   return (
     <>
       <main className={main}>
@@ -18,7 +20,8 @@ export default function Home({posts}) {
         </h1>
         <h2 className={errorMessage}>ngl idk where this page is</h2>
 
-        <PostList posts={sortPosts(posts)} />
+        <PostList posts={sortPosts(posts)} heading="Trash Reads" />
+        <PhotoList photos={photos} heading="Pretty Pictures" />
       </main>
     </>
   )
@@ -26,6 +29,8 @@ export default function Home({posts}) {
 
 export const getStaticProps = async () => ({
   props: {
-    posts: await Promise.all((await postsFns.getStaticPaths()).paths.map(postsFns.getStaticProps).slice(0, 4))
+    posts: await Promise.all((await postsFns.getStaticPaths()).paths.map(postsFns.getStaticProps).slice(0, 4)),
+    photos: (await photos()).slice(0, 8),
+    photoSize: process.env.UNSPLASH_SIZE
   }
 })
