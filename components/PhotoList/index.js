65a75fa21f419fd19b31651aef3226a743239a2a
Reuben <4957964+reubn@users.noreply.github.com>
2021-01-10 22:57:33 +0000

    🔥Reuse Image on Front Page

--- components/PhotoList/index.js
+++ components/PhotoList/index.js
@@ -1,6 +1,8 @@
 import ActiveLink from '../ActiveLink'
 
 import Section from '@/components/Section'
+import Image from '@/components/Image'
+
 import {photos as photosStyle, photo} from './styles'
 
 const PhotoList = ({photos: {photos, size}, fallback=null, ...props}) => (
@@ -9,8 +11,8 @@ const PhotoList = ({photos: {photos, size}, fallback=null, ...props}) => (
       <Section heading="Recent Photos" moreHref="https://unsplash.com/re" moreAria="More Photos" {...props}>
         <section className={photosStyle} style={{'--columns': Math.min(photos.length, 4), '--photo-size': `${size}px`}}>
           {photos.map(({src, id}) => (
-            <a href={`https://unsplash.com/photos/${id}`} className={photo} aria-label={`Unsplash Photo ${id}`} key={id}>
-              <img src={src} alt={`Unsplash Photo ${id}`} />
+            <a href={`https://unsplash.com/photos/${id}`} className={photo} aria-label="Unsplash Photo" key={id}>
+              <Image image={{src, size: {width: size}}} alt="Unsplash Photo" lazy={false} />
             </a>)
           )}
         </section>
