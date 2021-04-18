b13239a4a48c1f3818c0a05ed6c769bb0db8803f
Reuben <4957964+reubn@users.noreply.github.com>
2021-04-18 19:58:41 +0100

    🔥Switch to Unsplash Collection

--- src/getPhotos.js
+++ src/getPhotos.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
-const response = fetch(`https://api.unsplash.com/users/${process.env.UNSPLASH_USERNAME}/photos?client_id=${process.env.UNSPLASH_CLIENT_ID}&page=1&per_page=${process.env.UNSPLASH_COUNT}&order_by=latest`).then(res => res.json())
+const response = fetch(`https://api.unsplash.com/collections/${process.env.UNSPLASH_COLLECTION}/photos?client_id=${process.env.UNSPLASH_CLIENT_ID}&page=1&per_page=${process.env.UNSPLASH_COUNT}&order_by=latest`).then(res => res.json())
 
 export default async (...slice) => ({
 	size: process.env.UNSPLASH_SIZE,
 	photos: (
-		(await response)
+		(Array.isArray(await response) ? await response : [])
 		.slice(...slice)
 		.map(({id, urls: {raw}}) => ({id, src: `${raw}&q=${process.env.UNSPLASH_QUALITY}&fm=auto&w=${process.env.UNSPLASH_SIZE}&fit=max`}))
 	)
