526be2bc6b00ffcd3d1d1c01e21accc8aa5839aa
Reuben <4957964+reubn@users.noreply.github.com>
2021-08-02 15:48:16 +0100

    🐛 Keep Image Placeholder while Image Loads

--- components/Image/index.js
+++ components/Image/index.js
@@ -1,15 +1,15 @@
-import {useState, useRef} from 'react'
+import {useState} from 'react'
 
 import supportsWebp from '@/src/supportsWebp'
 
 import Lazy from '../Lazy'
 
-import {image as imageStyle, loading} from './styles'
+import {image as imageStyle, loading, notLoading, imageLoading, intersectionHandle} from './styles'
 
-const Image = ({image={}, className, lazy=true, ...props}) => {
+const Image = ({image={}, className, lazy=true, alt, ...props}) => {
   const {src='', srcSet='', size: {width, height}={}, id} = image
 
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(!lazy)
   const [useSrc, setUseSrc] = useState(src)
 
   const onError = async () => {
@@ -23,25 +23,32 @@ const Image = ({image={}, className, lazy=true, ...props}) => {
     setUseSrc(await decodeWebp(polyfillSrc))
   }
 
-  const imageFn = ({_ref, inView, ...lazyProps}) => (
-    <img
-      ref={_ref}
-      id={id}
+  const imageFn = ({inView, ...lazyProps}) => (
+    <>
+      <img
+        id={id}
 
-      onLoad={() => setLoaded(true)}
-      onError={() => inView && onError()}
+        onLoad={event => setLoaded(event.target.complete)}
+        onError={() => inView && onError()}
 
-      src={inView ? useSrc : ''}
-      srcSet={inView ? useSrc === src ? srcSet : undefined : ''}
+        src={(loaded || inView) ? useSrc : ''}
+        srcSet={(loaded || inView) ? useSrc === src ? srcSet : undefined : ''}
 
-      width={width}
-      height={height}
-      className={[imageStyle, className, loaded ? '' : loading].join(' ')}
-      loading="lazy"
+        width={width}
+        height={height}
+        className={[imageStyle, className, loaded ? '' : imageLoading].join(' ')}
+        loading={lazy ? "eager" : "lazy" /* we're handling the lazy loading, dw*/}
 
-      {...props}
-      {...lazyProps}
-    />
+        {...props}
+        {...lazyProps}
+      />
+      <span
+        key={id}
+        className={[className, loaded ? notLoading : loading].join(' ')}
+        style={{aspectRatio: `${width}/${height}`}}>
+          {alt}
+      </span>
+    </>
   )
 
   return (
