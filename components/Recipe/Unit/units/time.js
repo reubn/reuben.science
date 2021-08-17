f715ada1a59f837652603df8293b6701568599c3
Reuben <4957964+reubn@users.noreply.github.com>
2021-08-17 21:19:30 +0100

    🔥 Separate Time Units

--- components/Recipe/Unit/units/time.js
+++ components/Recipe/Unit/units/time.js
@@ -14,27 +14,37 @@ const timeUnits = {
   ms: {value: milisecond, identifier: 'ms', padding: 3, floor: false}
 }
 
-export default {
-  time: {
-    name: 'time',
-    colour: 'pink',
-    type: 'time',
-    isBase: true,
-    format: ({value: seconds}) => Object.entries(timeUnits).reduce(({seconds, components}, [key, {value, identifier, padding, floor=true}]) => {
-      const quantity = floor ? Math.floor(seconds / value) : seconds / value
-      const newSeconds = seconds - (quantity * value)
+const {d, h, m, s, ms} = timeUnits
 
-      return {
-        seconds: newSeconds,
-        components: quantity ? [
-          ...components,
-          ['value', (floor ? quantity : toFixedOrInteger(quantity, 2)).toString().padStart(components.length ? padding : 0, '0')],
-          ['unit', identifier],
-          ['raw', newSeconds ? ' ' : false]
-        ] : components
-      }
-    }, {seconds, components: []}).components,
-    parse: def => Object.entries(def).reduce((total, [key, quantity]) => total + (quantity * timeUnits[key].value), 0),
-    isComfortable: seconds => true
-  }
+const base = (timeUnits, config) => ({
+  ...config,
+  colour: 'pink',
+  type: 'time',
+  isBase: true,
+  base: Symbol(),
+  format: ({value: seconds}) => Object.entries(timeUnits).reduce(({seconds, components}, [key, {value, identifier, padding, floor=true}]) => {
+    const quantity = floor ? Math.floor(seconds / value) : seconds / value
+    const newSeconds = seconds - (quantity * value)
+
+    return {
+      seconds: newSeconds,
+      components: quantity ? [
+        ...components,
+        ['value', (floor ? quantity : toFixedOrInteger(quantity, 2)).toString().padStart(components.length ? padding : 0, '0')],
+        ['unit', identifier],
+        ['raw', newSeconds ? ' ' : false]
+      ] : components
+    }
+  }, {seconds, components: []}).components,
+  parse: def => Object.entries(def).reduce((total, [key, quantity]) => total + (quantity * (timeUnits[key]?.value || 0)), 0),
+  isComfortable: seconds => true
+})
+
+export default {
+  time: base({d, h, m, s, ms}, {name: 'time'}),
+  dhms: base({d, h, m, s}, {name: 'dhms'}),
+  dhm: base({d, h, m}, {name: 'dhm'}),
+  dh: base({d, h}, {name: 'dh'}),
+
+  hms: base({h, m, s}, {name: 'hms'}),
 }
