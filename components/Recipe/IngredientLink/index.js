7bb730037e1f5ac435008c8b049f95e5fad838c8
Reuben <4957964+reubn@users.noreply.github.com>
2021-08-19 12:41:34 +0100

    🔥 IngredientLink Multiple Ingredients

--- components/Recipe/IngredientLink/index.js
+++ components/Recipe/IngredientLink/index.js
@@ -2,38 +2,62 @@ import {useEffect, useState, useMemo} from 'react'
 
 import IngredientText from '../IngredientText'
 
-import {link, quantity, hover} from './styles'
+import {link, quantity, hover as hoverStyle, conjunction} from './styles'
 
-export const IngredientLink = ({ingredient, children}) => {
-  // console.log('RENDER LINK', ingredient.name)
+export const IngredientLink = ({ingredients, children}) => {
+  // console.log('RENDER LINK', ingredients.map(i => i.name))
+  const hover = ingredients.some(ingredient => ingredient.hover === ingredient.hover === 'definition')
 
   return (
     <span
-     className={[link, (ingredient.hover === 'definition') && hover].filter(cn => cn).join(' ')}
-     style={{'--ingredient-accent': `var(--colours-${ingredient.colour})`}}
-     onMouseEnter={() => ingredient.setHover('link')}
-     onMouseLeave={() => ingredient.setHover(false)}
+     className={[link, hover && hoverStyle].filter(cn => cn).join(' ')}
+     style={{'--ingredient-accent': `var(--colours-${ingredients[0].colour})`}}
      >
-      <IngredientText
-       ingredient={ingredient}
-       name={false}
-       interactive={true}
+      <span className={quantity}>
+        {ingredients.map((ingredient, index) => {
+          const ingredientText = (
+            <IngredientText
+             key={ingredient.id}
+             ingredient={ingredient}
+             name={false}
+             interactive={true}
 
-       className={quantity}
-      />
-      {children || ingredient.name.toLowerCase()}
+             onMouseEnter={() => ingredient.setHover('link')}
+             onMouseLeave={() => ingredient.setHover(false)}
+            />
+          )
+
+          if(index) return (
+            <>
+              <span className={conjunction}>or</span>
+              {ingredientText}
+            </>
+          )
+
+          return ingredientText
+
+        })}
+      </span>
+      <span
+       onMouseEnter={() => ingredients.forEach(ingredient => ingredient.setHover('link'))}
+       onMouseLeave={() => ingredients.forEach(ingredient => ingredient.setHover(false))}
+      >{children || ingredients[0].name.toLowerCase()}</span>
     </span>
   )
 }
 
-export const createIngredientLink = recipe => ({id, scale: localScale=1, ...props}) => {
-  const ingredient = recipe.getIngredient(id)
+export const createIngredientLink = recipe => ({id, ids=[id], scale: localScale=1, ...props}) => {
+  const ingredients = ids.map(id => {
+    const ingredient = recipe.getIngredient(id)
 
-  if(!ingredient) throw `Ingredient ${id} not defined`
+    if(!ingredient) throw `Ingredient ${id} not defined`
 
-  const locallyScaledIngredient = useMemo(
-    () => localScale === 1 ? ingredient : ingredient.withScale(localScale),
-    [ingredient, ingredient.displayUnit, ingredient.hover, recipe.scale, localScale]
+    return ingredient
+  })
+
+  const locallyScaledIngredients = useMemo(
+    () => localScale === 1 ? ingredients : ingredients.map(ingredient => ingredient.withScale(localScale)),
+    [...ingredients.flatMap(ingredient => [ingredient, ingredient.displayUnit, ingredient.hover]), recipe.scale, localScale]
   )
 
   const [_, setDummy] = useState()
@@ -46,7 +70,7 @@ export const createIngredientLink = recipe => ({id, scale: localScale=1, ...prop
   }, [])
 
   return useMemo(() => (
-    <IngredientLink ingredient={locallyScaledIngredient} {...props} />
-  ), [locallyScaledIngredient, locallyScaledIngredient.displayUnit, locallyScaledIngredient.hover, recipe.scale])
+    <IngredientLink ingredients={locallyScaledIngredients} {...props} />
+  ), [locallyScaledIngredients, recipe.scale])
 }
 export default createIngredientLink
