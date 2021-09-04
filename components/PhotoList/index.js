9a596130323faa47a20009b5a3234b292d6842ad
Reuben <4957964+reubn@users.noreply.github.com>
2021-09-04 11:36:26 +0100

    🔥 Refactor Columns

--- components/PhotoList/index.js
+++ components/PhotoList/index.js
@@ -9,8 +9,8 @@ const PhotoList = ({photos: {photos, width}, fallback=null, ...props}) => (
   photos.length
     ? (
       <Section heading="Recent Photos" moreHref="https://unsplash.com/re" moreAria="More Photos" {...props}>
-        <div className={photosStyle} style={{'--columns': Math.min(photos.length, 4), '--photo-width': `${width}px`}} role="grid">
           {photos.map(({id, desc, src, height}) => (
+        <div className={photosStyle} style={{'--photo-count': photos.length, '--photo-width': `${width}px`}} role="grid">
             <a href={`https://unsplash.com/photos/${id}`} className={photo} title={`Photo: ${desc}`} key={id}>
               <Image image={{src, size: {width, height}}} alt={`Photo: ${desc}`} lazy={false} />
             </a>)
