f9a175294995f59cf6d16bfb8aca64820dce9ef9
Pho <76738455+pho119@users.noreply.github.com>
2021-10-09 17:20:15 +0100

    🐾

--- pages/secret/index.js
+++ pages/secret/index.js
@@ -1,3 +1,5 @@
+import {NextSeo} from 'next-seo'
+
 import Video from '@/components/Video'
 import {main} from '../styles'
 
@@ -6,26 +8,29 @@ import fin from './finlay.webm'
 import {video} from './styles'
 
 export default () => (
-  <main className={main}>
-    <h1>Welcome to Finlay's Lair</h1>
-    <Video
-     className={video}
-     muted
-     autoPlay
-     loop
-     video={
-       {
-         size: {
-           width: 1280,
-           height: 720
-         },
-         sources: [{
-           src: fin,
-           type: 'video/webm'
-         }]
+  <>
+    <NextSeo noindex={true} nofollow={true}/>
+    <main className={main}>
+      <h1>Welcome to Finlay's Lair</h1>
+      <Video
+       className={video}
+       muted
+       autoPlay
+       loop
+       video={
+         {
+           size: {
+             width: 1280,
+             height: 720
+           },
+           sources: [{
+             src: fin,
+             type: 'video/webm'
+           }]
+         }
        }
-     }
-    />
-    <p>🐾</p>
-  </main>
+      />
+      <p>🐾</p>
+    </main>
+  </>
 )
