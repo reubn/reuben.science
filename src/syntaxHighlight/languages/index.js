65d52ffdc9a74b469bd3b0722f21ecf2bfa80173
Reuben <4957964+reubn@users.noreply.github.com>
2022-03-24 19:36:53 +0000

    📝

--- src/syntaxHighlight/languages/index.js
+++ src/syntaxHighlight/languages/index.js
@@ -2,6 +2,7 @@ const languages = [
   require('./bashExtra'),
   require('./txtExtra'),
   require('./jsonExtra'),
+  require('./applescriptExtra')
 ]
 
 module.exports = refractor => languages.forEach(language => refractor.register(language))
