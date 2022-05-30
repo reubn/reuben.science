d3b88decf84f806ba2a883b81e7920fbbd60e8fe
Reuben <4957964+reubn@users.noreply.github.com>
2022-05-30 20:16:13 +0100

    🧹

--- buildScripts/extractPages.js
+++ buildScripts/extractPages.js
@@ -1,22 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-const {PRERENDER_MANIFEST} = require('next/constants')
-const manifest = require(path.join(process.cwd(), '.next', PRERENDER_MANIFEST))
+const {PRERENDER_MANIFEST, EXPORT_DETAIL} = require('next/constants')
+const {outDirectory} = require(path.join(process.cwd(), '.next', EXPORT_DETAIL))
+const preRenderManifest = require(path.join(process.cwd(), '.next', PRERENDER_MANIFEST))
 
-const extractPages = () => {
-  const {routes} = manifest
+const {routes: prmRoutes} = preRenderManifest
 
-  return Object.entries(routes).flatMap(([page, route]) => {
-    const {pageProps} = require(path.join(process.cwd(), 'out', route.dataRoute))
-
-    if(pageProps.hidden) return []
-
-    return {
-      page,
-      pageProps
-    }
-  })
-}
+const getPageProps = dataRoute => require(path.join(outDirectory, dataRoute)).pageProps || {}
+const extractPages = () => Object.entries(prmRoutes).map(([page, {dataRoute}]) => ({page, pageProps: getPageProps(dataRoute)}))
 
 module.exports = extractPages
