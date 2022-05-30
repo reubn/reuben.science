1d5485434b3f0685c191923d8386a69f821560a8
Reuben <4957964+reubn@users.noreply.github.com>
2022-05-30 20:17:32 +0100

    🔥 Conceal Pages

--- buildScripts/after.js
+++ buildScripts/after.js
@@ -5,7 +5,11 @@ env.loadEnvConfig(process.cwd(), process.env.NODE_ENV === 'development')
 const extractPages = require('./extractPages')
 const generateSitemap = require('./generateSitemap')
 const generateRSS = require('./generateRSS')
+const concealPages = require('./concealPages')
 
 const pages = extractPages()
-generateSitemap(pages)
-generateRSS(pages)
+
+const safePages = concealPages(pages)
+
+generateSitemap(safePages)
+generateRSS(safePages)
