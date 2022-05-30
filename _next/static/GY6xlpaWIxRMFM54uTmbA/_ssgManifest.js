6254ba659d4df868fea3f47a27f8d55cb3b0bdc7
reubn <reubn@users.noreply.github.com>
2022-05-30 19:22:40 +0000

    Deploying to gh-pages from @ reubn/reuben.science@9cc941a5cd92cada466476b0076fc4e3776e4e2c 🚀

---
+++ _next/static/GY6xlpaWIxRMFM54uTmbA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["/categories","/snippets","/404","/","/posts","/posts/[slug]","/snippets/[slug]","/categories/[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/P7-N_SwRN3z41U3rQvlIp/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcategories","\u002Fsecret","\u002Fsnippets","\u002F404","\u002F","\u002Fposts","\u002Fposts\u002F[slug]","\u002Fsnippets\u002F[slug]","\u002Fcategories\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
