bba403be54e989d6da9c09e0ef152ab3831fc37e
Reuben <4957964+reubn@users.noreply.github.com>
2022-06-05 17:26:14 +0100

    🔥 References

--- src/refs/rehype.mjs
+++ src/refs/rehype.mjs
@@ -1,7 +1,24 @@
 import {visit} from 'unist-util-visit'
 
 function visitor(node, index, parent) {
-  if(node.tagName === 'li' && node.properties && node.properties.id && node.properties.id.startsWith('fn-')) node.properties['ref-number'] = node.properties.id.replace(/^fn-/, '')
+  if(node.tagName === 'li' && node.properties?.id?.startsWith('user-content-fn-')) {
+    const refNumber = node.properties.id.replace(/^user-content-fn-/, '')
+    node.properties['ref-number'] = refNumber
+    node.properties.id = `def-${refNumber}`
+  }
+
+  // Defintion
+  if(node.tagName === 'a' && node.properties?.dataFootnoteBackref) {
+    const refNumber = node.properties?.href?.replace(/^#user-content-fnref-/, '')
+    node.properties.href = `#ref-${refNumber}`
+  }
+
+  // Reference
+  if(node.tagName === 'a' && node.properties?.dataFootnoteRef) {
+    const refNumber = node.properties?.id?.replace(/^user-content-fnref-/, '')
+    node.properties.id = `ref-${refNumber}`
+    node.properties.href = `#def-${refNumber}`
+  }
 }
 
 export default () => tree => visit(tree, 'element', visitor)
