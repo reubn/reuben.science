6a2cb57cd5ef4c44927d704b3c070aba7e8e373d
Reuben <4957964+reubn@users.noreply.github.com>
2022-06-06 20:35:14 +0100

    🔥 Ref Fixes

--- src/refs/rehype.mjs
+++ src/refs/rehype.mjs
@@ -5,19 +5,29 @@ function visitor(node, index, parent) {
     const refNumber = node.properties.id.replace(/^user-content-fn-/, '')
     node.properties['ref-number'] = refNumber
     node.properties.id = `def-${refNumber}`
-  }
+
+    // remove intermediate p
+    const pChild = node.children[1]
+    node.children = pChild.children
+  } else
 
   // Defintion
   if(node.tagName === 'a' && node.properties?.dataFootnoteBackref) {
     const refNumber = node.properties?.href?.replace(/^#user-content-fnref-/, '')
     node.properties.href = `#ref-${refNumber}`
-  }
+  } else
 
   // Reference
   if(node.tagName === 'a' && node.properties?.dataFootnoteRef) {
     const refNumber = node.properties?.id?.replace(/^user-content-fnref-/, '')
     node.properties.id = `ref-${refNumber}`
     node.properties.href = `#def-${refNumber}`
+  } else
+
+  if(node.tagName === 'h2' && node.properties?.id === 'footnote-label') {
+    node.tagName = 'hr'
+    node.children = []
+    node.properties = {}
   }
 }
 
