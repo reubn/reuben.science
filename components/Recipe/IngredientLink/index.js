f3fe6f87910b06b892579171c666c96df84d50a9
Reuben <4957964+reubn@users.noreply.github.com>
2022-06-08 23:04:29 +0100

    🐛

--- components/Recipe/IngredientLink/index.js
+++ components/Recipe/IngredientLink/index.js
@@ -20,7 +20,6 @@ export const IngredientLink = ({ingredient, quantityOnly=false, inlineQuantity=q
          interactive={true}
 
          className={!inlineQuantity && quantity}
-         style={{display: typeof window !== 'undefined' ? undefined : 'none'}}
         />
       )
     }
