805fabd4a377b01d6a9aa202ed751261ac0a185d
Reuben <4957964+reubn@users.noreply.github.com>
2022-06-11 16:42:56 +0100

    🔥 ARIA

--- components/PhotoList/index.js
+++ components/PhotoList/index.js
@@ -9,9 +9,9 @@ const PhotoList = ({photos: {photos, width, maxHeight}, fallback=null, ...props}
   photos.length
     ? (
       <Section heading="Recent Photos" moreHref="https://unsplash.com/re" moreAria="More Photos" {...props}>
-        <div className={photosStyle} style={{'--photo-count': photos.length, '--photo-width': `${width}px`}} role="grid">
+        <div className={photosStyle} style={{'--photo-count': photos.length, '--photo-width': `${width}px`}} role="list">
           {photos.map(({id, desc, resolutions}) => (
-            <a href={`https://unsplash.com/photos/${id}`} className={photo} title={`Photo: ${desc}`} key={id}>
+            <a href={`https://unsplash.com/photos/${id}`} className={photo} title={`Photo: ${desc}`} key={id} role="listitem">
               <Image image={{id, resolutions}} alt={`Photo: ${desc}`} lazy={false} aspectRatio={`${resolutions[1].width}/${resolutions[1].width * Math.min(maxHeight / width, resolutions[1].height / resolutions[1].width)}`} sizes="(max-width: 360px) calc(min(1000px, 100vw - 3.5rem) - 1rem), (max-width: 800px) calc((min(1000px, 100vw - 3.5rem) - 1rem) / 2), (max-width: 900px) calc((min(1000px, 100vw - 3.5rem) - 1rem) / 3), calc((min(1000px, 100vw - 3.5rem) - 1rem) / 4)" />
             </a>)
           )}
