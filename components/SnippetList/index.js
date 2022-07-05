a2e20a05d69da894cf190d81264b937cd5ae71b3
Reuben <4957964+reubn@users.noreply.github.com>
2022-07-05 13:19:51 +0100

    🐛

--- components/SnippetList/index.js
+++ components/SnippetList/index.js
@@ -6,7 +6,7 @@ import {posts as postsStyle} from '../SnippetList/styles'
 const SnippetList = ({posts, fallback=null, ...props}) => (
   posts.length
     ? (
-      <Section heading="Recent Snippets" moreHref="/posts" moreAria="More Snippets" {...props}>
+      <Section heading="Recent Snippets" moreHref="/snippets" moreAria="More Snippets" {...props}>
         <div className={postsStyle} role="feed">
           {posts.map(props => <SnippetLink key={props.slug} {...props} />)}
         </div>
