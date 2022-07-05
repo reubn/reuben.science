6981b7443c3c8fd3a3acdc8cf02b1ae993a2f0cc
Reuben <4957964+reubn@users.noreply.github.com>
2022-07-06 00:36:51 +0100

    🔥 Toggle Snippet Preview

--- components/SnippetLink/index.js
+++ components/SnippetLink/index.js
@@ -3,15 +3,19 @@ import ActiveLink from '../ActiveLink'
 import {post as postStyle, title as titleStyle, description as descriptionStyle, emoji as emojiStyle, readingTime as readingTimeStyle} from '../PostLink/styles'
 import {post as snippetPostStyle, pre} from './styles'
 
-const SnippetLink = ({slug, metadata: {title, description, emoji, linesOfCode, preview: {html}}, ...props}) => {
+const SnippetLink = ({slug, metadata: {title, description, emoji, linesOfCode, preview: {html}}, displayPreview, ...props}) => {
+  const preview = displayPreview && (
+    <pre className={pre} role="img" aria-label="Snippet Preview">
+      <div>
+        <code dangerouslySetInnerHTML={{__html: html}}/>
+      </div>
+    </pre>
+  )
+
   const post = active => (
     <a title={title} {...props}>
       <article className={`${postStyle} ${snippetPostStyle}`}>
-        <pre className={pre} role="img" aria-label="Snippet Preview">
-          <div>
-            <code dangerouslySetInnerHTML={{__html: html}}/>
-          </div>
-        </pre>
+        {preview}
         <h1>
           <p className={emojiStyle}>{emoji}</p>
           <p className={titleStyle}>{title}</p>
--- components/SnippetList/index.js
+++ components/SnippetList/index.js
@@ -3,12 +3,12 @@ import SnippetLink from '@/components/SnippetLink'
 
 import {posts as postsStyle} from '../SnippetList/styles'
 
-const SnippetList = ({posts, fallback=null, ...props}) => (
+const SnippetList = ({posts, fallback=null, displayPreview, ...props}) => (
   posts.length
     ? (
       <Section heading="Recent Snippets" moreHref="/snippets" moreAria="More Snippets" {...props}>
         <div className={postsStyle} role="feed">
-          {posts.map(props => <SnippetLink key={props.slug} {...props} />)}
+          {posts.map(props => <SnippetLink key={props.slug} displayPreview={displayPreview} {...props} />)}
         </div>
       </Section>
     )
