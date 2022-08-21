b3f0bd15d52d9056b8989374938d6305897805af
Reuben <4957964+reubn@users.noreply.github.com>
2022-08-21 15:43:26 +0100

    🧹

--- components/MultiStepCalculation/MultiStepCalculationInput.js
+++ components/MultiStepCalculation/MultiStepCalculationInput.js
@@ -198,7 +198,7 @@ export const MultiStepCalculationInput = ({node, title, type, emphasis, userInpu
       inputRef={inputRef}
 
       style={{
-        '--placeholder-size': flags.userInputNeeded ? 'var(--input-size' : undefined,
+        '--placeholder-size': flags.waiting ? '0.8rem' : undefined,
         '--placeholder-size-active': flags.waiting ? 'var(--placeholder-size)' : undefined,
         '--placeholder-colour': flags.ignored ? 'var(--colours-dark-3)' : undefined,
         '--title-colour': flags.ignored ? 'var(--colours-dark-5)' : undefined,
